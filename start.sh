d3a6a013de05725c6cdd65c2dd905b480fead20c
Node <nodar.chkuaselidze@gmail.com>
2017-07-30 20:50:36 +0400

    Move configs to ENV vars

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+server=$(which http-server)
+
+: ${HOST:=0.0.0.0}
+: ${PORT:=8000}
+: ${RPC_URL:=http://localhost:8545}
+
+echo $HOST:$PORT $RPC_URL
+
+cat ./app/app.js.template | sed "s#{WEB3_RPC_URL}#$RPC_URL#;" > ./app/app.js
+
+$server ./app -a $HOST -p $PORT
